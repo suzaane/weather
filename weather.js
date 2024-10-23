1ea06d736ec9244da2fbcd5b71933dae52e20fe8
Suzaane <suzaynshah@gmail.com>
2024-10-23 19:59:31 +0545

    Update weather.js
    replaced my api key

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-const apiKey = '5fa7777aaadcad39f3641eb8bc560d61';
+const apiKey = 'YOUR_API_KEY';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const locationInput = document.getElementById('inputarea');
